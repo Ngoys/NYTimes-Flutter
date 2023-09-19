f9d9405b9c3e00fa0d48ab5631339182754eb24f
Shawn Ngo <YenSern.Shawn@sicpa.com>
2023-09-19 11:53:41 +0800

    [NYTimes-Flutter] Regenerate DI files

--- lib/config/app_injector.config.dart
+++ lib/config/app_injector.config.dart
@@ -9,13 +9,14 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i6;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:nytimes/service/geolocation_store.dart' as _i4;
-import 'package:nytimes/service/remote/api/api_service.dart' as _i3;
-import 'package:nytimes/service/endpoint_provider.dart' as _i7;
-import 'package:nytimes/service/user_store.dart' as _i5;
+import 'package:nytimes/service/article_store.dart' as _i8;
+import 'package:nytimes/service/geolocation_store.dart' as _i3;
+import 'package:nytimes/service/remote/api/api_client.dart' as _i6;
+import 'package:nytimes/service/remote/api/base_dio.dart' as _i7;
+import 'package:nytimes/service/user_store.dart' as _i4;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -28,17 +29,15 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    final apiService = _$ApiService();
-    gh.singleton<_i3.AppDio>(apiService.init());
-    gh.lazySingleton<_i4.GeoLocationStore>(() => _i4.GeoLocationStore());
-    gh.lazySingleton<_i5.UserStore>(
-        () => _i5.UserStore(secureStorage: gh<_i6.FlutterSecureStorage>()));
-    gh.singleton<_i7.BaseStore>(_i7.BaseStore(
-      gh<_i3.AppDio>(),
-      gh<_i5.UserStore>(),
-    ));
+    gh.lazySingleton<_i3.GeoLocationStore>(() => _i3.GeoLocationStore());
+    gh.lazySingleton<_i4.UserStore>(
+        () => _i4.UserStore(secureStorage: gh<_i5.FlutterSecureStorage>()));
+    gh.lazySingleton<_i6.APIClient>(() => _i6.APIClient(
+          gh<_i7.AppDio>(),
+          gh<_i4.UserStore>(),
+        ));
+    gh.lazySingleton<_i8.ArticleStore>(
+        () => _i8.ArticleStore(apiClient: gh<_i6.APIClient>()));
     return this;
   }
 }
-
-class _$ApiService extends _i3.ApiService {}
